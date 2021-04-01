d570c278b425113deb2761288b780f8db090b021
Adham Garcia <agarcia@itatix.com>
2021-04-01 08:43:27 -0600

    Revert "Em department Fix"
    This reverts commit 4782f82c289bda7478a4ca9946e42972ad7db6f3.

--- quickbooks_odoo_connector/model/quick_emp_department.py
+++ quickbooks_odoo_connector/model/quick_emp_department.py
@@ -207,8 +207,8 @@ class quickbook_department(models.Model):
             parent_id = rec['ParentRef']['value']
             values = self.get_ids(arguments, backend_id,
                                   filters, record_id=int(parent_id))
-            if values:
-             self.department_import_mapper(backend_id, data=values)
+            if values:'%20STARTPOSITION%20' + str(filters['count']) + '%20MAXRESULTS%20' + str(300) +
+                self.department_import_mapper(backend_id, data=values)
 
     @api.model
     def default_get(self,fields):
