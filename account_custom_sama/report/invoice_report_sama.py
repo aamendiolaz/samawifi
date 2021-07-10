d4294d2e0de2f59a68d04e76280b7ed49a18e101
Adham Garcia <agarcia@itatix.com>
2021-07-09 21:15:45 -0500

    fix

--- account_custom_sama/report/invoice_report_sama.py
+++ account_custom_sama/report/invoice_report_sama.py
@@ -211,9 +211,9 @@ class InvoiceReportSama(models.Model):
                 # print('__domain', __domain)
                 if not __domain:
                     __domain = domain
-                target_lines = self.env['sales.target.lines'].search(__domain)
-                amount_target = sum(target_lines.mapped('monthly_target'))
-                line['amount_target'] = amount_target
+                # target_lines = self.env['sales.target.lines'].search(__domain)
+                # amount_target = sum(target_lines.mapped('monthly_target'))
+                line['amount_target'] = 10.0
             except:
                 line['amount_target'] = 9.0
 
